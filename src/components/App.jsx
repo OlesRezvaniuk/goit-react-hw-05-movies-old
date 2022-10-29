fa6b1bc7126a3f2688d98268e618fc979a01875d
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-10-29 10:40:38 +0300

    r
    r

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './Home';
-
 export const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
   );
 };
--- src/components/Home.js
+++
@@ -1,7 +0,0 @@
-export const Home = () => {
-  return (
-    <div
-      style={{ display: 'block', height: '80px', backgroundColor: 'grey' }}
-    ></div>
-  );
-};
