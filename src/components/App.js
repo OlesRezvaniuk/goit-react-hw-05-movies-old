c305adb742733953770f8e9dbf9a8e3c758e2a0a
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-10-30 13:49:49 +0200

    r
    r

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { Layout } from './Layout';
 import { Trendings } from './Trendings/Trendings';
 import { MovieDetails } from './MovieDetails/MovieDetails';
 import { Cast } from './Cast/Cast';
-// import { Reviews } from './Reviews/Reviews';
+import { Reviews } from './Reviews/Reviews';
 
 export const App = () => {
   return (
@@ -13,7 +13,7 @@ export const App = () => {
         <Route path="home" element={<Trendings />} />
         <Route path="home/:id" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<div>HYI</div>} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
         <Route path="movies" element={<div>Movies</div>} />
       </Route>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,37 @@
 import { useParams } from 'react-router-dom';
-// import axios from 'axios';
-// import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 export const Reviews = () => {
   const { id } = useParams();
-  console.log(id);
-  return <div>Reviews</div>;
+  const [reviews, setReviews] = useState([]);
+
+  const getReviewsApi = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=6f552eb6929f0128999ddb3bd491ac60&language=en-US&page=1`
+    );
+    return data;
+  };
+
+  const onArrayItems = async () => {
+    const movieReview = await getReviewsApi();
+    setReviews(movieReview.results);
+  };
+
+  console.log(reviews);
+
+  useEffect(() => {
+    onArrayItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <ul>
+      {reviews.map(review => {
+        <li>
+          <p>{review.author}</p>
+        </li>;
+      })}
+    </ul>
+  );
 };
